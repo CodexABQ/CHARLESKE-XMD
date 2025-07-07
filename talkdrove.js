37c28c1ea69249daed93c6713aa6b71bdeb54317
Qaasim <149349613+CodexABQ@users.noreply.github.com>
2025-07-07 06:25:38 +0100

    Update talkdrove.js

--- talkdrove.js
+++ talkdrove.js
@@ -9,7 +9,7 @@
     "env": {
       "SESSION_ID": {
         "description": "Put your SESSION_ID here. Make sure it starts with ey~  or get session id from here,  https://charle-ke.onrender.com/",
-        "value":"",
+        "value":"eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRUZrSUd2UStmdDhleC82d0pLd2dpVzEwMHMzeTZrcDJteUdKNjFtdWMzUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicDkzU0pKd0IxUm9nYk1RcEg2cDlxc0lWd2VCamlyRExlSjZVaGczNklUdz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI4TE5TR0JYMnowdDZBWTdYdkt2MFdFMkdEYlMxNHRJa2lDQVphMkNsNFZnPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI2a3FTUmhSYURUYzBPZmwxVEVkOHJJdFgweVVBaVlsSERJTGxWb0J1REY0PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IldBQjJPL0NjUWU0NGVIV0c4WUVES1JQZk5pRGFKV00zVVlDYWQzMkNFRjg9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Imo5cG4vT0UzNmswNlhBWXBTdTNyeGl5TGNiVm1HZzdkejVPMGI5Z3JoSG89In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoid0pndWdreHlyeTdyNzlFS3FHdUFSczQvZmh3VlBremVObTI5WjIvQmZXYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUzNwbjhTSld6ZWdMK2w3WkhDckgwYnlMRTlEVGVQc0lVa2xCQy85Ulp3WT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InlXYnJzSWFmbGpHVlBWT1EzVmUwTHd1Tzdaa3oySmJzaW1RdUE3dEZxNlFhMlRhTkFlZEtUQTJua216SW8zQnFTMExnb2hMQjA0MVRWV1ZZd2VnbGdRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6NTcsImFkdlNlY3JldEtleSI6Iml2bis3d1I1dDdFRGVqZ28vWnpIYzVZSTZwZnhobC84WWt3S0czaFJJclE9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbeyJrZXkiOnsicmVtb3RlSmlkIjoiMjM0ODEyNzY2NjEzOUBzLndoYXRzYXBwLm5ldCIsImZyb21NZSI6dHJ1ZSwiaWQiOiJEOTk2NUFCRjgyRERGNzhCRUREMjAyOTg0MTgyQUEyRSJ9LCJtZXNzYWdlVGltZXN0YW1wIjoxNzUxODY1MTYzfSx7ImtleSI6eyJyZW1vdGVKaWQiOiIyMzQ4MTI3NjY2MTM5QHMud2hhdHNhcHAubmV0IiwiZnJvbU1lIjp0cnVlLCJpZCI6IkU0OEM4NjI2Qzk2NzY2NkNDRjYwMTlFQTMyMjExN0JCIn0sIm1lc3NhZ2VUaW1lc3RhbXAiOjE3NTE4NjUxNjN9XSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjEsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJyZWdpc3RlcmVkIjp0cnVlLCJwYWlyaW5nQ29kZSI6IjEyM0xPVFVTIiwibWUiOnsiaWQiOiIyMzQ4MTI3NjY2MTM5OjVAcy53aGF0c2FwcC5uZXQiLCJsaWQiOiIxNTU1ODU3MjcxODUwODk6NUBsaWQiLCJuYW1lIjoiLT3igqrbqdue26nigqo9IOKIgtGU0LzPg863ID3igqrbqdue26nigqo9LSJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDSXpNdGpFUW1LNnR3d1lZQXlBQUtBQT0iLCJhY2NvdW50U2lnbmF0dXJlS2V5Ijoib3hUMXRFc0l1aU1yQWUrL1M3ZXdyYlkwa1o1TUFVWUxHV0dmL1BsM0hEZz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiYUVkeUZ2eFR6ZnVUMWpvVUl6SmZGSWg5ZUpDVGVTRGlUbjhLcHVhZURpNjAzLzZla2FzQVBteUMvNTdNbTJWSkFmMDk3cS9Mc01SNnEzU1FmbEVUQ1E9PSIsImRldmljZVNpZ25hdHVyZSI6IkllM0VQWXlqTW9sdUpERWdkczRiU1ZPMTJ2MkpNcDFOTEdJV2lqTEF6Vk9QcGdFYjNGYXRGOGVOWUsxRi9kbnBGUXZOUVMxcjViMmJlb21oQmFQU2hRPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjM0ODEyNzY2NjEzOTo1QHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmFNVTliUkxDTG9qS3dIdnYwdTNzSzIyTkpHZVRBRkdDeGxobi96NWR4dzQifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJyb3V0aW5nSW5mbyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkNBSUlDQT09In0sImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTc1MTg2NTEyNSwibGFzdFByb3BIYXNoIjoiMkc0QW11IiwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFNQUUifQ==",
         "required": true        
     },
       "PREFIX": {
@@ -25,7 +25,7 @@
     "MODE": {
       "description": "select your bot work type public-private-inbox-group.",
       "required": false,
-      "value": "public"      
+      "value": "private"      
     },        
     "ALWAYS_ONLINE": {
       "description": "Make it true if want always online.",
@@ -65,7 +65,7 @@
     "OWNER_NUMBER": {
       "description": "put the owner number for bot.",
       "required": false,
-      "value": "254759626063"
+      "value": "2348127666139"
    }, 
     "BOT_NAME": {
       "description": "Type here the bot name.",
